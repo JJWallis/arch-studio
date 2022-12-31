987803e147a6be8a0bc937c5cc01013e57e11699
JJWallis <josh.jamesonwallis@gmail.com>
2022-12-31 10:51:42 +0000

    CopySectionWithBorderHome done pre big grey title

--- src/components/copySectionWithBorder/CopySectionWithBorderHome.tsx
+++ src/components/copySectionWithBorder/CopySectionWithBorderHome.tsx
@@ -1,37 +1,56 @@
-import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 import { device } from '../../config/theme/device';
 import CopySectionWithBorder from './CopySectionWithBorder';
-import WelcomeImage from '../../../public/assets/home/desktop/image-welcome.jpg';
 import { CopySectionWithBorderTitle } from './CopySectionWithBorderTitle';
 
 const Wrapper = styled(CopySectionWithBorder)`
    @media ${device.laptop} {
-      --gap: 125px;
+      --gap: 80px;
       display: grid;
-      grid-template-columns: 1fr auto;
-      gap: 125px;
+      grid-template-columns: minmax(400px, 1fr) minmax(200px, 350px); // 350px === welcome image native width
+      gap: var(--gap);
+   }
+
+   @media ${device.laptopL} {
+      --gap: 125px;
+   }
+`;
+
+const ContentWrapper = styled.div`
+   display: grid;
+   justify-items: center;
+`;
+
+const Content = styled.div`
+   @media ${device.laptop} {
+      max-width: 446px;
    }
 `;
 
 const Title = styled(CopySectionWithBorderTitle)`
-   outline: 1px solid black;
    margin-bottom: 22px;
 
    @media ${device.tablet} {
+      margin-bottom: 43px;
       max-width: 400px;
    }
 `;
 
 const Paragraph = styled.p`
    color: var(--clr-mild-grey);
+
    &:not(:last-child) {
       margin-bottom: 15px;
    }
 `;
 
-const LaptopImage = styled(Image)`
+const WelcomeImage = styled.div`
+   background-image: url('/assets/home/desktop/image-welcome.jpg');
+   background-repeat: no-repeat;
+   background-position: center;
+   background-size: cover;
+
    @media ${device.maxLaptop} {
       display: none;
    }
@@ -40,29 +59,29 @@ const LaptopImage = styled(Image)`
 const CopySectionWithBorderHome = () => {
    return (
       <Wrapper variant="home">
-         <div>
-            <Title>Welcome to Arch Studio</Title>
-            <Paragraph>
-               We have a unique network and skillset to help bring your projects
-               to life. Our small team of highly skilled individuals combined
-               with our large network put us in a strong position to deliver
-               exceptional results.
-            </Paragraph>
-            <Paragraph>
-               Over the past 10 years, we have worked on all kinds of projects.
-               From stations to high-rise buildings, we create spaces that
-               inspire and delight.
-            </Paragraph>
-            <Paragraph>
-               We work closely with our clients so that we understand the
-               intricacies of each project. This allows us to work in harmony
-               the surrounding area to create truly stunning projects that will
-               stand the test of time.
-            </Paragraph>
-         </div>
-         <div>
-            <LaptopImage src={WelcomeImage} alt="" />
-         </div>
+         <ContentWrapper>
+            <Content>
+               <Title>Welcome to Arch Studio</Title>
+               <Paragraph>
+                  We have a unique network and skillset to help bring your
+                  projects to life. Our small team of highly skilled individuals
+                  combined with our large network put us in a strong position to
+                  deliver exceptional results.
+               </Paragraph>
+               <Paragraph>
+                  Over the past 10 years, we have worked on all kinds of
+                  projects. From stations to high-rise buildings, we create
+                  spaces that inspire and delight.
+               </Paragraph>
+               <Paragraph>
+                  We work closely with our clients so that we understand the
+                  intricacies of each project. This allows us to work in harmony
+                  the surrounding area to create truly stunning projects that
+                  will stand the test of time.
+               </Paragraph>
+            </Content>
+         </ContentWrapper>
+         <WelcomeImage />
       </Wrapper>
    );
 };
