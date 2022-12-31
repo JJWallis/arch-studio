e3059c5cd7cb1430c0c971bc16a462ee466ea14e
JJWallis <josh.jamesonwallis@gmail.com>
2022-12-31 09:07:01 +0000

    variants setup for copy section with border

--- src/components/copySectionWithBorder/CopySectionWithBorder.tsx
+++ src/components/copySectionWithBorder/CopySectionWithBorder.tsx
@@ -12,47 +12,71 @@ const Wrapper = styled.section<WrapperProps>`
    ${({ variant }) =>
       ({
          home: css`
-            --marin-top: 72px;
-            --margin-bottom: 112px; // todo -> all different for tablet only (styled())
+            --margin-top: 72px;
+            --margin-bottom: 112px;
          `,
          about: css`
-            --marin-top: 76px;
+            --margin-top: 76px;
             --margin-bottom: 112px;
          `,
          contact: css`
-            --marin-top: 100px;
+            --margin-top: 100px;
             --margin-bottom: 71px;
          `,
       }[variant])}
    --padding-top: 69px;
-
    position: relative;
    padding-top: var(--padding-top);
    padding-inline: var(--padding-x, 0);
-   margin-top: var(--marin-top);
-
-   @media ${device.maxTablet} {
-      --padding-x: var(--site-padding);
-   }
-
-   @media ${device.tablet} {
-      --padding-top: 50px; // todo -> zero for Home on tablet + desktop (styled())
-      --marin-top: 200px;
-   }
-
-   @media ${device.laptop} {
-      --padding-top: 83px;
-      --margin-bottom: 200px;
-   }
+   margin-top: var(--margin-top);
 
    &::before {
       content: '';
       position: absolute;
       top: 0;
       display: block;
-      height: 1px;
       width: 65px;
-      background: var(--clr-light-grey);
+      height: 1px;
+      background-color: var(--clr-light-grey);
+   }
+
+   @media ${device.maxTablet} {
+      --padding-x: var(--site-padding);
+   }
+
+   @media ${device.tablet} {
+      --padding-top: 50px;
+      --margin-top: 200px;
+   }
+
+   @media ${device.laptop} {
+      --padding-top: 83px;
+      --margin-bottom: 200px;
+   }
+`;
+
+const WrapperHome = styled(Wrapper)`
+   @media ${device.tablet} {
+      --padding-top: 0;
+      &::before {
+         display: none;
+      }
+   }
+
+   @media ${device.tablet} and ${device.maxLaptop} {
+      --margin-bottom: 233px;
+   }
+`;
+
+const WrapperAbout = styled(Wrapper)`
+   @media ${device.tablet} and ${device.maxLaptop} {
+      --margin-bottom: 207px;
+   }
+`;
+
+const WrapperContact = styled(Wrapper)`
+   @media ${device.tablet} and ${device.maxLaptop} {
+      --margin-bottom: 226px;
    }
 `;
 
@@ -63,9 +87,20 @@ interface CopySectionWithBorderProps {
 
 const CopySectionWithBorder: React.FC<CopySectionWithBorderProps> = ({
    children,
+   variant,
    ...props
 }) => {
-   return <Wrapper {...props}>{children}</Wrapper>;
+   const Component = {
+      home: WrapperHome,
+      about: WrapperAbout,
+      contact: WrapperContact,
+   }[variant];
+
+   return (
+      <Component variant={variant} {...props}>
+         {children}
+      </Component>
+   );
 };
 
 export default CopySectionWithBorder;
