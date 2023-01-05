ad89a18be605b6049f027280ac11d7d909eae867
JJWallis <josh.jamesonwallis@gmail.com>
2023-01-05 19:29:51 +0000

    gallery section done

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 import { device } from '../../config/theme/device';
 import { ROUTE_PORTFOLIO } from '../../constants/routes';
 import { ArrowAndCopyButton } from '../button/ArrowAndCopyButton';
 import { contentWrapperStyles } from '../styled/ContentWrapperStyles';
+import StaticImage from '../../../public/assets/portfolio/mobile/image-228b.jpg';
+import Link from 'next/link';
 
 const Wrapper = styled.section`
-   outline: solid;
    ${contentWrapperStyles}
    margin-block: 73px;
    @media ${device.maxTablet} {
@@ -40,12 +42,74 @@ const SeeAllButton = styled(ArrowAndCopyButton)`
    }
 `;
 
+const Gallery = styled.div.attrs({ role: 'grid' })`
+   display: grid;
+   grid-template-columns: 1fr;
+`;
+
+const GalleryPiece = styled.div.attrs({ role: 'grid-cell' })`
+   display: grid;
+   grid-template-columns: 1fr;
+
+   &::before {
+      content: '';
+      position: relative;
+      z-index: 1;
+      background-color: rgba(0, 0, 0, 0.15);
+      grid-column: 1 / -1;
+      grid-row: 1 / -1;
+      height: 100%;
+      width: 100%;
+      pointer-events: none;
+   }
+`;
+
+const GalleryPieceImage = styled(Image)`
+   grid-column: 1 / -1;
+   grid-row: 1 / -1;
+`;
+
+const GalleryPieceContent = styled.div`
+   --padding: 0 0 5% 7%;
+   position: relative;
+   z-index: 2;
+   grid-column: 1 / -1;
+   grid-row: 1 / -1;
+   align-self: flex-end;
+   padding: var(--padding);
+
+   @media ${device.tablet} {
+      --padding: 0 0 40px 40px;
+   }
+`;
+
+const GalleryPieceTitle = styled.h3`
+   font-size: var(--fs-heading-small);
+   font-weight: 700;
+   color: white;
+`;
+
+const GalleryPieceLink = styled(Link)`
+   color: white;
+`;
+
 const Featured: React.FC = () => (
    <Wrapper>
       <Header>
          <Title>Featured</Title>
          <SeeAllButton href={ROUTE_PORTFOLIO}>See All</SeeAllButton>
       </Header>
+      <Gallery>
+         <GalleryPiece>
+            <GalleryPieceImage src={StaticImage} alt="" />
+            <GalleryPieceContent>
+               <GalleryPieceTitle>228B Tower</GalleryPieceTitle>
+               <GalleryPieceLink href={ROUTE_PORTFOLIO}>
+                  View All Projects
+               </GalleryPieceLink>
+            </GalleryPieceContent>
+         </GalleryPiece>
+      </Gallery>
    </Wrapper>
 );
 
